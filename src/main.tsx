7cfbc512c9da7eb331c46adae720794bb4a143eb
2019xiaopeng <2019xiaopeng@users.noreply.github.com>
2026-05-04 07:06:34 +0000

    style(weread): add base stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
+import "./ui/weread.css";
 import { installFrontendLogging, logFrontend } from "./tauri/frontendLog";
 
 installFrontendLogging();
